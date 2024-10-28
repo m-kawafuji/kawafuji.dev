c9b76b84326e1b6b1608aeaa413c68d88c9b8c4b
Masashi Kawafuji <masashikawafuji@proton.me>
2024-10-29 00:15:28 +0900

    Update Stylelint configuration

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -3,6 +3,7 @@ const stylelintConfig = {
   extends: ['stylelint-config-standard-scss'],
   plugins: ['stylelint-order'],
   rules: {
+    'selector-class-pattern': null,
     'order/properties-alphabetical-order': true,
   },
 };
