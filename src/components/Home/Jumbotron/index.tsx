4e759980dbf9bc9c78d7c7ea80dc4a4f2eae49a2
Masashi Kawafuji <masashikawafuji@proton.me>
2024-12-11 07:29:46 +0900

    Change Jumbotron styles

--- src/components/Home/Jumbotron/index.tsx
+++ src/components/Home/Jumbotron/index.tsx
@@ -1,21 +1,16 @@
-import Container from '@/components/Container';
 import styles from './index.module.scss';
 
 export default function Jumbotron() {
   return (
-    <div className={styles.container}>
-      <Container>
-        <section className={styles.inner}>
-          <h1 className={styles.title}>
-            Welcome to
-            <br />
-            Masashi Kawafuji&apos;s Blog
-          </h1>
-          <p className={styles.description}>
-            This is my personal blog where I mainly write about web development.
-          </p>
-        </section>
-      </Container>
-    </div>
+    <section className={styles.container}>
+      <h1 className={styles.title}>
+        Welcome to
+        <br />
+        Masashi Kawafuji&apos;s Blog
+      </h1>
+      <p className={styles.description}>
+        This is my personal blog where I mainly write about web development.
+      </p>
+    </section>
   );
 }
