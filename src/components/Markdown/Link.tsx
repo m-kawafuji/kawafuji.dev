251b1354209237bf5c6fcacebcb5c5b98af1e4de
Masashi Kawafuji <masashikawafuji@proton.me>
2024-12-11 07:47:01 +0900

    Fix props type

--- src/components/Markdown/Link.tsx
+++ src/components/Markdown/Link.tsx
@@ -1,6 +1,9 @@
 import NextLink from 'next/link';
 
-export default function Link({ href, ...props }: JSX.IntrinsicElements['a']) {
+export default function Link({
+  href,
+  ...props
+}: React.JSX.IntrinsicElements['a']) {
   if (href?.startsWith('/')) {
     return <NextLink href={href} {...props} />;
   }
