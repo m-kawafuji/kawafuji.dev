e7a6dea9e0eca527ab1ef36ca57482984dca36fa
Masashi Kawafuji <masashikawafuji@proton.me>
2024-12-11 07:31:15 +0900

    Remove Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,13 @@
 import Container from '@/components/Container';
 import Jumbotron from '@/components/Home/Jumbotron';
-import Section from '@/components/Home/Section';
 import PostList from '@/components/Home/PostList';
-import styles from './page.module.scss';
 
 export default function Home() {
   return (
     <main>
       <Container>
         <Jumbotron />
-        <div className={styles.sections}>
-          <Section title="Posts">
-            <PostList />
-          </Section>
-        </div>
+        <PostList />
       </Container>
     </main>
   );
