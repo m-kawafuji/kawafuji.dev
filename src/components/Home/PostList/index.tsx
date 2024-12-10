a63601a8f479d67e38ef91b9c6131af643e2b947
Masashi Kawafuji <masashikawafuji@proton.me>
2024-12-11 07:51:03 +0900

    Fix heading

--- src/components/Home/PostList/index.tsx
+++ src/components/Home/PostList/index.tsx
@@ -14,11 +14,11 @@ export default async function PostList() {
     <div className={styles.list}>
       {posts.map((post) => (
         <article key={post.metadata.slug}>
-          <h3 className={styles.title}>
+          <h2 className={styles.title}>
             <Link href={`/blog/${post.metadata.slug}/`} className={styles.link}>
               {post.metadata.title}
             </Link>
-          </h3>
+          </h2>
           <p className={styles.date}>
             <time dateTime={post.metadata.date}>
               {format(post.metadata.date, 'PPP')}
