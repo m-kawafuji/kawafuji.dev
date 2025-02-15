7e097a48962d8a83ec42ea182d7a1d5c28764f54
Masashi Kawafuji <masashikawafuji@proton.me>
2025-02-16 01:25:08 +0900

    Change className to class

--- 404.tsx
+++ 404.tsx
@@ -5,10 +5,10 @@ export const stylesheet = "/styles/404/index.css";
 export default function NotFound() {
   return (
     <main>
-      <div className="l-container">
-        <section className="p-errorContent">
-          <h1 className="p-errorContent__title">Page Not Found</h1>
-          <p className="p-errorContent__description">
+      <div class="l-container">
+        <section class="p-errorContent">
+          <h1 class="p-errorContent__title">Page Not Found</h1>
+          <p class="p-errorContent__description">
             The requested page was not found.
           </p>
         </section>
